6424a10fe12625e38d15b852dca504a03b7742f5
Balaji <balaji0017@gmail.com>
2024-05-31 13:41:16 +0100

    final framework1

--- src/test/java/com/cgi/pages/LoginPage.java
+++ src/test/java/com/cgi/pages/LoginPage.java
@@ -4,31 +4,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-	
-	
+
+	private By usernameLocator = By.name("username");
+	private By passwordLocator = By.xpath("//input[@placeholder='Password']");
+	private By loginLocator = By.xpath("//button[normalize-space()='Login']");
+	private By errorLocator=By.xpath("//p[contains(normalize-space(),'Invalid')]");
+
 	private WebDriver driver;
-	
-	public LoginPage(WebDriver driver)
-	{
-		this.driver=driver;
+
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
 	}
-	
+
 	public void enterUsername(String username) {
-		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(usernameLocator).sendKeys(username);
 	}
 
 	public void enterPassword(String password) {
-		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(passwordLocator).sendKeys(password);
 	}
-	
-	//clickOnLogin
-	public void clickOnLogin()
-	{
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+
+	// clickOnLogin
+	public void clickOnLogin() {
+		driver.findElement(loginLocator).click();
 	}
-	
-	public String getInvalidErrorMessage()
-	{
-		return driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
+
+	public String getInvalidErrorMessage() {
+		return driver.findElement(errorLocator).getText();
+	}
+
+	public String getUsernamePlaceholder() {
+		return driver.findElement(usernameLocator).getAttribute("placeholder");
 	}
 }
--- src/test/java/com/cgi/pages/PIMPage.java
+++ src/test/java/com/cgi/pages/PIMPage.java
@@ -1,11 +1,17 @@
 package com.cgi.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class PIMPage {
+	private By addEmployeeLocator=By.linkText("Add Employee");
+	
 	private WebDriver driver;
 
 	public PIMPage(WebDriver driver) {
 		this.driver = driver;
 	}
+	
+	
+	
 }
